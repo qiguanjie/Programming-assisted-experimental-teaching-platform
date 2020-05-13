5afc10d6ad3c5fcadbe4a1f25c4c96699625a5de
qiguan <qiguanjiezl@163.com>
2020-05-13 23:18:10 +0800

    md测试

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 import pymysql
 from config import db
-from decorators import login_limit
+# from decorators import login_limit
 import time
 import config
 
@@ -129,7 +129,7 @@ def blog_art(art):
 
 # 个人中心
 @app.route('/personal')
-@login_limit
+# @login_limit
 def personal():
     cur = db.cursor()
     usernameGet = session.get('user_id')
@@ -145,21 +145,21 @@ def personal():
 
 # 个人中心我的关注
 @app.route('/personal/attention')
-@login_limit
+# @login_limit
 def personal_attention():
     return render_template('personal_attention.html')
 
 
 # 个人中心 我的博客
 @app.route('/personal/blog')
-@login_limit
+# @login_limit
 def personal_blog():
     return render_template('personal_blog.html')
 
 
 # 个人中心 修改个人信息
 @app.route('/personal/change', methods=['GET', 'POST'])
-@login_limit
+# @login_limit
 def personal_change():
     if request.method == 'GET':
         cur = db.cursor()
@@ -192,14 +192,14 @@ def personal_change():
 
 # 个人中心 我的收藏
 @app.route('/personal/collect')
-@login_limit
+# @login_limit
 def personal_collect():
     return render_template('personal_collect.html')
 
 
 # 个人中心 我的粉丝
 @app.route('/personal/fans')
-@login_limit
+# @login_limit
 def personal_fans():
     return render_template('personal_fans.html')
 
@@ -275,9 +275,9 @@ def formula():
             raise e
 
 
-# 发布问答
+# 发布问答 - 默认 - 富文本编辑器
 @app.route('/formula/post_questions', methods=['GET', 'POST'])
-@login_limit
+# @login_limit
 def post_questions():
     if request.method == 'GET':
         return render_template('formula_post_question.html')
@@ -285,8 +285,8 @@ def post_questions():
         try:
             cur = db.cursor()
             email = session.get('user_id')
-            title = request.form.get('question_title')
-            content = request.form.get('question_content')
+            title = request.form.get('title')
+            content = request.form.get('editorValue')
             date = time.strftime("%Y-%m-%d %H:%M:%S")
             sql = "select max(formula_id) from SDWZCS.formula_post"
             db.ping(reconnect=True)
@@ -307,6 +307,46 @@ def post_questions():
             cur.execute(sql)
             db.commit()
             return redirect(url_for('formula'))
+        except Exception as e:
+            raise e
+
+# 发布问答 - Markdown编辑器
+@app.route('/formula/post_question_md', methods=['GET', 'POST'])
+def post_question_md():
+    if request.method == 'GET':
+        return render_template('post_question_md.html')
+    if request.method == 'POST':
+        try:
+            cur = db.cursor()
+            email = session.get('user_id')
+            title = request.form.get('title')
+            content = request.form.get('html_content')
+            print(request.values)
+            # 如果未登录 则跳转到登录页面
+            if email is None:
+                return redirect(url_for('login'))
+            date = time.strftime("%Y-%m-%d %H:%M:%S")
+            sql = "select max(formula_id) from SDWZCS.formula_post"
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            result = cur.fetchone()[0]
+            if result is None:
+                formula_id = 1
+            else:
+                formula_id = int(result) + 1
+            sql = "insert into SDWZCS.formula_post(SDWZCS.formula_post.formula_id, SDWZCS.formula_post.author, " \
+                  "SDWZCS.formula_post.title,SDWZCS.formula_post.creat_time) values ('%s','%s','%s','%s')" % (
+                      formula_id, email, title, date)
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            db.commit()
+            sql = "insert into question_detail(formula_id, qno, content, datetime, author) VALUES ('%s','1','%s','%s','%s')" % (
+            formula_id, content, date, email)
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            db.commit()
+            return redirect(url_for('formula'))
+
         except Exception as e:
             raise e
 
--- decorators.py
+++ decorators.py
@@ -3,13 +3,13 @@ from flask import session, url_for, redirect
 
 
 # 登录限制
-def login_limit(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if session.get('user_id'):
-            return func(*args, **kwargs)
-        else:
-            return redirect(url_for('login'))
-        return func(*args, **kwargs)
-    return wrapper
+# def login_limit(func):
+    # @wraps(func)
+    # def wrapper(*args, **kwargs):
+    #     if session.get('user_id'):
+    #         return func(*args, **kwargs)
+    #     else:
+    #         return redirect(url_for('login'))
+    #     return func(*args, **kwargs)
+    # return wrapper
 
